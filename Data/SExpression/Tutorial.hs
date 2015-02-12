63ca71a4927178db0f59ca314b19ff35dcc0009a
Getty Ritter <gettylefou@gmail.com>
2015-02-12 01:05:14 -0800

    Beginning implementation of Rivest flavor; beginnings of Tutorial module

---
+++ Data/SExpression/Tutorial.hs
@@ -0,0 +1,35 @@
+{-| The @blah@ library attempts to be as general as possible, and to
+    support a wide range of use-cases for s-expressions. It is built
+    around a core of primitives which are then exposed in various
+    ways, and can be easily and flexibly extended. This tutorial
+    describes particular use-cases, and then shows how to adapt this
+    library to that use-case.
+-}
+
+module Data.SExpression.Tutorial
+  ( -- * Basic Usage and Organization
+    -- $usage
+    -- * Analyzing Scheme code
+    -- $scheme
+    -- * Building a Custom Config Format
+    -- $config
+    -- * Building a Custom Lisp
+    -- $lisp
+  ) where
+
+{- $usage
+
+-}
+
+{- $scheme
+
+-}
+
+
+{- $config
+
+-}
+
+{- $lisp
+
+-}
