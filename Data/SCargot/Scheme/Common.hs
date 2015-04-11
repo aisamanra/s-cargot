77b2bd3a6d41f72928cee575d9231b595d00db69
Getty Ritter <gdritter@galois.com>
2015-04-11 15:17:24 -0700

    Beginnings of Scheme support

---
+++ Data/SCargot/Scheme/Common.hs
@@ -0,0 +1,9 @@
+module Data.SCargot.Scheme.Common where
+
+-- Schemes are actually kind of complicated! The goal is to support
+-- R[34567]RS pretty fully, and I'm actually 100% happy to try to
+-- add R[12]RS support, as well, but that might actually involve
+-- hunting down physical copies of the relevant reports.
+
+-- R3RS is dedicated to the memory of Algol-60, so I might have
+-- to dedicate this to the memory of R(<6)RS.
